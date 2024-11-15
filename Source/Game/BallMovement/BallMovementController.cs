8c7c20a7385e89fbe46df3e859c66c3229fc6a04
Zacoriot <isaacjbrooks@gmail.com>
2024-11-15 15:22:39 +0000

    Created Input Manager

--- Source/Game/BallMovement/BallMovementController.cs
+++ Source/Game/BallMovement/BallMovementController.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using FlaxEngine;
+﻿using FlaxEngine;
 
 namespace Game;
 
@@ -9,27 +7,8 @@ namespace Game;
 /// </summary>
 public class BallMovementController : Script
 {
-    /// <inheritdoc/>
-    public override void OnStart()
-    {
-        // Here you can add code that needs to be called when script is created, just before the first game update
-    }
-    
-    /// <inheritdoc/>
-    public override void OnEnable()
-    {
-        // Here you can add code that needs to be called when script is enabled (eg. register for events)
-    }
-
-    /// <inheritdoc/>
-    public override void OnDisable()
-    {
-        // Here you can add code that needs to be called when script is disabled (eg. unregister from events)
-    }
-
-    /// <inheritdoc/>
     public override void OnUpdate()
     {
-        // Here you can add code that needs to be called every frame
+        Debug.Log(InputManager.GetMovementAxis());
     }
 }
---
+++ Source/Game/Managers/InputManager.cs
@@ -0,0 +1,30 @@
+﻿using FlaxEngine;
+
+namespace Game;
+
+/// <summary>
+/// InputManager Script.
+/// </summary>
+public class InputManager : Script
+{
+    // INPUT AXIS
+    static InputAxis _VerticalMovementAxis;
+    static InputAxis _HorizontalMovementAxis;
+
+    public override void OnAwake()
+    {
+        _VerticalMovementAxis = new InputAxis("Vertical");
+        _HorizontalMovementAxis = new InputAxis("Horizontal");
+    }
+
+    public override void OnDestroy()
+    {
+        _VerticalMovementAxis.Dispose();
+        _HorizontalMovementAxis.Dispose();
+    }
+
+    public static Vector2 GetMovementAxis()
+    {
+        return new Vector2(_HorizontalMovementAxis.ValueRaw, _VerticalMovementAxis.ValueRaw);
+    }
+}
