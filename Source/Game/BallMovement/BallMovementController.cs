d2086715ba71094ebe89dbe9773dc79e55de4b24
Zacoriot <isaacjbrooks@gmail.com>
2024-11-22 15:37:15 +0000

    Got ball rolling and initial jump

--- Source/Game/BallMovement/BallMovementController.cs
+++ Source/Game/BallMovement/BallMovementController.cs
@@ -7,8 +7,42 @@ namespace Game;
 /// </summary>
 public class BallMovementController : Script
 {
-    public override void OnUpdate()
+    RigidBody _RigidBody;
+
+    [Header("==MOVEMENT==")]
+    [ShowInEditor, Serialize] float _RollSpeed;
+
+    [Header("==JUMP==")]
+    [ShowInEditor, Serialize] float _JumpForce;
+
+    public override void OnStart()
     {
-        Debug.Log(InputManager.GetMovementAxis());
+        _RigidBody = (RigidBody)Actor;
+    }
+
+    public override void OnEnable()
+    {
+        InputManager.GetJump().Pressed += Jump;
+    }
+
+    public override void OnFixedUpdate()
+    {
+        Vector3 finalForce = new Vector3(InputManager.GetMovementAxis().X, 
+                                        0, 
+                                        InputManager.GetMovementAxis().Y);
+
+        finalForce = finalForce.Normalized * _RollSpeed;
+
+        _RigidBody.AddForce(finalForce);
+    }
+
+    public override void OnDisable()
+    {
+        InputManager.GetJump().Pressed -= Jump;
+    }
+
+    void Jump()
+    {
+       _RigidBody.AddForce(Vector3.Up * _JumpForce);
     }
 }
--- Source/Game/Managers/InputManager.cs
+++ Source/Game/Managers/InputManager.cs
@@ -11,20 +11,33 @@ public class InputManager : Script
     static InputAxis _VerticalMovementAxis;
     static InputAxis _HorizontalMovementAxis;
 
+    // INPUT ACTIONS
+    static InputEvent _Jump;
+
     public override void OnAwake()
     {
+        // INPUT AXIS
         _VerticalMovementAxis = new InputAxis("Vertical");
         _HorizontalMovementAxis = new InputAxis("Horizontal");
+
+        // INPUT ACTIONS
+        _Jump = new InputEvent("Jump");
     }
 
     public override void OnDestroy()
     {
+        // INPUT AXIS
         _VerticalMovementAxis.Dispose();
         _HorizontalMovementAxis.Dispose();
+
+        // INPUT EVENTS
+        _Jump.Dispose();
     }
 
     public static Vector2 GetMovementAxis()
     {
         return new Vector2(_HorizontalMovementAxis.ValueRaw, _VerticalMovementAxis.ValueRaw);
     }
+
+    public static InputEvent GetJump() => _Jump;
 }
