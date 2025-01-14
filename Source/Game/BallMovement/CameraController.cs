9a9a619b501d627516f1b48a110721a050e3348b
Isaac Brooks <isaacjbrooks@gmail.com>
2025-01-14 10:20:53 +0000

    camera

---
+++ Source/Game/BallMovement/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using FlaxEngine;
+
+namespace Game;
+
+/// <summary>
+/// CameraController Script.
+/// </summary>
+public class CameraController : Script
+{
+    [ShowInEditor, Serialize] Actor _Ball;
+    [ShowInEditor, Serialize] float _FollowSpeed;
+    [ShowInEditor, Serialize] Vector3 _Offset;
+
+    public override void OnLateUpdate()
+    {
+        Actor.Position = Vector3.Lerp(Actor.Position, _Ball.Position + _Offset, Time.DeltaTime * _FollowSpeed);
+        Actor.LookAt(_Ball.Position);
+    }
+}
